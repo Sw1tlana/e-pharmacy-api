48b7e8f166560a3553404af47442859fd4591273
Sw1tlana <127736251+Sw1tlana@users.noreply.github.com>
2024-12-15 12:01:33 +0200

    changes productsServices

--- services/productsServices.js
+++ services/productsServices.js
@@ -19,7 +19,7 @@ export const getMedicinesProductsServises = async (filter= {}, page, limit) => {
 
 export const getProductDetailsServices = async (productId) => {
   try {
- const product = await Product.findById({ productId } );
+ const product = await Product.findById(productId);
  return product;
 
   } catch(error) {
